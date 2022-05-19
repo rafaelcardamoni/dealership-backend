966b37fadb6ebf82698a8a6e446d988be77b3a59
Rafael Cardamoni <rafael.cardamoni@gmail.com>
2022-05-19 16:40:45 -0300

    changed delete car route to delete images from aws and database before deleting the car

--- src/services/cars/DeleteCarService.ts
+++ src/services/cars/DeleteCarService.ts
@@ -1,14 +1,35 @@
-import { response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { CarsRepository } from '../../repositories/CarsRepository';
+import { ImagesRepository } from '../../repositories/ImagesRepository';
+import aws from 'aws-sdk';
 
 class DeleteCarService {
   async execute(id: string) {
     const carsRepository = getCustomRepository(CarsRepository);
+    const imagesRepository = getCustomRepository(ImagesRepository);
 
     if (!(await carsRepository.findOne(id))) {
       return new Error('Invalid ID');
     }
+
+    const images = await imagesRepository.find({
+      select: ['id', 'filename'],
+      where: {
+        car_id: id
+      }
+    });
+
+    images.forEach(async image => {
+      // deleting image from aws
+      const s3 = new aws.S3();
+      s3.deleteObject({
+        Bucket: 'dealership-project-upload-1',
+        Key: image.filename
+      }).promise();
+
+      // deleting image from database
+      await imagesRepository.delete(image.id);
+    });
 
     await carsRepository.delete(id);
   }
